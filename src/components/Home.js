d5f96436c20d79cbb91334c83286b69afa7f0aa7
Lailz <lailaabdulraheem@gmail.com>
2020-11-10 20:01:42 +0300

    add home component

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+// Styles
+import { Title, Description, ShopImage } from "../styles";
+
+const Home = () => {
+  return (
+    <div>
+      <Title>Cookies and Beyond</Title>
+      <Description>Where cookie maniacs gather</Description>
+      <ShopImage
+        src="https://i.pinimg.com/originals/8f/cf/71/8fcf719bce331fe39d7e31ebf07349f3.jpg"
+        alt="Cookie Shop"
+      />
+    </div>
+  );
+};
+
+export default Home;
